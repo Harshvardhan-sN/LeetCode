2f70bc853241adb179ebaa94535eb163cb7f6414
Harshvardhan Singh Negi <81630801+Harshvardhan-sN@users.noreply.github.com>
2022-01-22 00:17:14 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (18.43%) - LeetHub

--- 290-word-pattern/290-word-pattern.cpp
+++ 290-word-pattern/290-word-pattern.cpp
@@ -5,12 +5,23 @@ public:
         unordered_map<string,int> m2;
         vector<string> v1;
         int n = pattern.length();
-        
-        string word;
-        stringstream s1(s);
-        while(s1>>word){
-            v1.emplace_back(word);
+        s += ' ';
+        string  word = "";
+        for(int i=0;i<s.length();i++){
+            if(s[i]==' '){
+                v1.emplace_back(word);
+                word="";
+            }
+            else{
+                word+=s[i];
+            }
         }
+        
+        // string word;
+        // stringstream s1(s);
+        // while(s1>>word){
+        //     v1.emplace_back(word);
+        // }
         if(n!=v1.size())    return false;
         
         for(int i=0;i<n;i++){
