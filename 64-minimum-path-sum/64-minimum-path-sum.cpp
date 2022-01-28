f406eb279063600782ff867304704a859b1582ec
Harshvardhan Singh Negi <81630801+Harshvardhan-sN@users.noreply.github.com>
2022-01-28 20:21:18 +0530

    Time: 4 ms (98.88%), Space: 9.9 MB (57.48%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.cpp
+++ 64-minimum-path-sum/64-minimum-path-sum.cpp
@@ -1,26 +1,46 @@
 #define ll long long
 class Solution {
 public:
-    ll dp[207][207];
+//     ll dp[207][207];
+
+//     ll solve(ll start,ll end,vector<vector<int>> &v1){
+//         if(start==0 && end==0)  return v1[start][end];
+//         if(start<0 || end<0)    return INT_MAX;
+
+//         if(dp[start][end]!=-1)  return dp[start][end];
 
 
-    ll solve(ll start,ll end,vector<vector<int>> &v1){
-        if(start==0 && end==0)  return v1[start][end];
-        if(start<0 || end<0)    return INT_MAX;
+//         ll up = solve(start-1,end,v1) + v1[start][end];
+//         ll left = solve(start,end-1,v1)  + v1[start][end];
 
-        if(dp[start][end]!=-1)  return dp[start][end];
-
-
-        ll up = solve(start-1,end,v1) + v1[start][end];
-        ll left = solve(start,end-1,v1)  + v1[start][end];
-
-        return dp[start][end] = min(up,left);
-    }
+//         return dp[start][end] = min(up,left);
+//     }
 
     int minPathSum(vector<vector<int>>& grid) {
-        memset(dp,-1,sizeof(dp));
+        // memset(dp,-1,sizeof(dp));
         ll n = grid.size();
         ll m = grid[0].size();
-        return solve(n-1,m-1,grid);
+        // return solve(n-1,m-1,grid);
+        int arr[n][m];
+        memset(arr,0,sizeof(arr));
+    
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(i==0 && j==0)	arr[i][j] = grid[i][j];
+                else{
+                
+                    int up = 0;
+                    if(i>0)		up+=grid[i][j] + arr[i-1][j];
+                    else	up+=1e7;
+                
+                    int left = 0;
+                    if(j>0)		left+=grid[i][j] + arr[i][j-1];
+                    else 	left+=1e7;
+                
+                    arr[i][j] = min(up,left);
+                }
+            }
+        }
+        return arr[n-1][m-1];
     }
 };
