71de5569e1c36dc69ec7306aa44b8b7d07e153e5
Harshvardhan Singh Negi <81630801+Harshvardhan-sN@users.noreply.github.com>
2022-01-31 16:59:58 +0530

    Time: 13 ms (11.57%), Space: 7.8 MB (86.51%) - LeetHub

--- 1672-richest-customer-wealth/1672-richest-customer-wealth.cpp
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.cpp
@@ -1,12 +1,26 @@
+// class Solution {
+// public:
+//     int maximumWealth(vector<vector<int>>& accounts) {
+//         int maxi = 0;
+//         for(auto &it:accounts){        
+//             int sum = accumulate(it.begin(),it.end(),0);
+//             maxi = max(maxi,sum);
+            
+//         }
+//         return maxi;
+//     }
+// };
 class Solution {
 public:
     int maximumWealth(vector<vector<int>>& accounts) {
-        int maxi = 0;
-        for(auto &it:accounts){        
-            int sum = accumulate(it.begin(),it.end(),0);
-            maxi = max(maxi,sum);
-            
+        int res = 0;
+        for(int i =0;i<accounts.size();i++){
+            int temp = 0;
+            for(int j = 0;j<accounts[i].size();j++){
+                temp+=accounts[i][j];
+            }
+            res = max(res,temp);
         }
-        return maxi;
+        return res;
     }
 };
