fb10e3032e3fb1e8ba0be08267dd04e7fb926fb1
Harshvardhan Singh Negi <81630801+Harshvardhan-sN@users.noreply.github.com>
2022-04-15 13:58:30 +0530

    Time: 7 ms (62.12%), Space: 8.3 MB (43.09%) - LeetHub

--- 338-counting-bits/338-counting-bits.cpp
+++ 338-counting-bits/338-counting-bits.cpp
@@ -1,10 +1,18 @@
 class Solution {
 public:
+    long f(long i){
+        long c = 0;
+        while(i){
+            c++;
+            i &= (i-1);
+        }
+        return c;
+    }
     vector<int> countBits(int n) {
         long i = 0;
         vector<int> v1;
         while(i<=n){
-            v1.push_back(__builtin_popcountll(i));
+            v1.push_back(f(i));
             i++;
         }
         return v1;
