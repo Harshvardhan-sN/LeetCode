f397719def8723ad229c8a40aa26053e94077bb8
Harshvardhan Singh Negi <81630801+Harshvardhan-sN@users.noreply.github.com>
2022-05-07 13:51:38 +0530

    Time: 14 ms (5.14%), Space: 9.6 MB (94.80%) - LeetHub

--- 35-search-insert-position/35-search-insert-position.cpp
+++ 35-search-insert-position/35-search-insert-position.cpp
@@ -1,21 +1,8 @@
 class Solution {
 private:
-    int fun(vector<int> &v1,int start,int end,int x){
-        int ans = 0;
-        while(start<=end){
-            int mid = start+(end-start)/2;
-            if(v1[mid]>=x){
-                end = mid-1;
-            }
-            else       start = mid+1;
-        }
-        return start;
-    }
+    
 public:
     int searchInsert(vector<int>& nums, int target) {
-        int l = 0;
-        int r = nums.size()-1;
-        // return fun(nums,l,r,target);
         return lower_bound(nums.begin(),nums.end(),target) - nums.begin();
     }
 };
