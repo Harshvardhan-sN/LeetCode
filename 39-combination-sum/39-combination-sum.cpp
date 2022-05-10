6a727fd34e942a1ba1888fb7ff538254a58e2b37
Harshvardhan Singh Negi <81630801+Harshvardhan-sN@users.noreply.github.com>
2022-05-10 19:57:38 +0530

    Time: 104 ms (21.78%), Space: 67.3 MB (12.51%) - LeetHub

---
+++ 39-combination-sum/39-combination-sum.cpp
@@ -0,0 +1,22 @@
+class Solution {
+    vector<vector<int>> ans;
+private:
+    void sum(int index,int target,vector<int> help,vector<int> &v1){
+        if(index==help.size()){
+            if(target==0)   ans.push_back(v1);
+            return;
+        }
+        if(help[index]<=target){
+            v1.push_back(help[index]);
+            sum(index,target-help[index],help,v1);
+            v1.pop_back();
+        }
+        sum(index+1,target,help,v1);
+    }
+public:
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<int> m1;
+        sum(0,target,candidates,m1);
+        return ans;
+    }
+};
