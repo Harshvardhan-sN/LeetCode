9a9540dc6302c2e23c1cc63b222fd11288c29da0
Harshvardhan Singh Negi <81630801+Harshvardhan-sN@users.noreply.github.com>
2022-05-15 13:11:55 +0530

    Time: 42 ms (7.14%), Space: 20.3 MB (7.14%) - LeetHub

--- 2273-find-resultant-array-after-removing-anagrams/2273-find-resultant-array-after-removing-anagrams.cpp
+++ 2273-find-resultant-array-after-removing-anagrams/2273-find-resultant-array-after-removing-anagrams.cpp
@@ -1,23 +1,23 @@
 class Solution {
-// private:
-//     bool check(string &s1,string &s2){
-//         map<char,int> m1,m2;
-//         for(auto &it:s1)    m1[it]++;
-//         for(auto &it:s2)    m2[it]++;
-//         return m1==m2;
-//     }
+private:
+    bool check(string &s1,string &s2){
+        map<char,int> m1,m2;
+        for(auto &it:s1)    m1[it]++;
+        for(auto &it:s2)    m2[it]++;
+        return m1==m2;
+    }
 public:
     vector<string> removeAnagrams(vector<string>& words) {
-        // for(int i=0;i<words.size()-1;i++){
-        //     if(check(words[i],words[i+1])){
-        //         words.erase(words.begin()+1+i--);
-        //     }
-        // }
         for(int i=0;i<words.size()-1;i++){
-            if(multiset<char>(begin(words[i]),end(words[i]))==multiset<char>(begin(words[i+1]),end(words[i+1]))){
+            if(check(words[i],words[i+1])){
                 words.erase(words.begin()+1+i--);
             }
         }
+        // for(int i=0;i<words.size()-1;i++){
+        //     if(multiset<char>(begin(words[i]),end(words[i]))==multiset<char>(begin(words[i+1]),end(words[i+1]))){
+        //         words.erase(words.begin()+1+i--);
+        //     }
+        // }
         return words;
     }
 };
