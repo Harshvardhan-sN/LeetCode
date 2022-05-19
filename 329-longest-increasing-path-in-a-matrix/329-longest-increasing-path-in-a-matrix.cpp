aea5fe51fc2528605037d85fdef823b083e947c1
Harshvardhan Singh Negi <81630801+Harshvardhan-sN@users.noreply.github.com>
2022-05-19 18:18:42 +0530

    Time: 48 ms (84.44%), Space: 15 MB (88.63%) - LeetHub

--- 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.cpp
+++ 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.cpp
@@ -1,14 +1,20 @@
 class Solution {
+    // Possible directions
     int dX[4] = {0,0,1,-1};
     int dY[4] = {1,-1,0,0};
+    // DP array   // Globaly declared array are intialized with ZEROs
     int dp[207][207];
+    
 private:
     int find(vector<vector<int>> &m1,int x,int y,int row,int col){
         if(dp[x][y])    return dp[x][y];
         int ans = 1;
         for(int i=0;i<4;i++){
+            // New_Points
             int dx = x + dX[i];
             int dy = y + dY[i];
+            
+            // Checking out_of_bound and valid_move   (Next > Current)
             if(dx>=0 and dy>=0 and dx<row and dy<col){
                 if(m1[dx][dy]>m1[x][y]){
                     ans = max(ans,1+find(m1,dx,dy,row,col));
@@ -17,6 +23,7 @@ private:
         }
         return dp[x][y] = ans;
     }
+    
 public:
     int longestIncreasingPath(vector<vector<int>>& matrix) {
         int ans = 0;
