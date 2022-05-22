1009eb7d77ea9a033d8a287037ec307d9bab9f3f
Harshvardhan Singh Negi <81630801+Harshvardhan-sN@users.noreply.github.com>
2022-05-22 12:34:22 +0530

    Time: 469 ms (33.33%), Space: 115.8 MB (33.33%) - LeetHub

--- 2279-maximum-bags-with-full-capacity-of-rocks/2279-maximum-bags-with-full-capacity-of-rocks.cpp
+++ 2279-maximum-bags-with-full-capacity-of-rocks/2279-maximum-bags-with-full-capacity-of-rocks.cpp
@@ -2,20 +2,22 @@ class Solution {
 public:
     int maximumBags(vector<int>& capacity, vector<int>& rocks, int k) {
         int ans = 0;
-        vector<int> v1;
+        multiset<int> v1;
         int n = rocks.size();
+        
         for(int i=0;i<n;i++){
             int diff = capacity[i]-rocks[i];
             if(diff==0)     ans++;
-            else    v1.push_back(diff);
+            else    v1.insert(diff);
         }
-        sort(v1.begin(),v1.end());
-        for(int i=0;i<v1.size();i++){
-            if(v1[i]<=k and k>0){
-                k -= v1[i];
+        
+        for(auto &it:v1){
+            if(it<=k and k>0){
+                k -= it;
                 ans++;
-            }   
+            }
         }
+        
         return ans;
     }
 };
