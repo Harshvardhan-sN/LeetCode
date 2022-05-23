b4a794f2c42456cbbfb2042a6fd73a4f4705392b
Harshvardhan Singh Negi <81630801+Harshvardhan-sN@users.noreply.github.com>
2022-05-23 09:51:36 +0530

    Time: 64 ms (98.85%), Space: 8.7 MB (98.79%) - LeetHub

--- 474-ones-and-zeroes/474-ones-and-zeroes.cpp
+++ 474-ones-and-zeroes/474-ones-and-zeroes.cpp
@@ -1,23 +1,42 @@
+// // DP RECURSIVE
+// class Solution {
+//     int dp[607][107][107];
+// private:
+//     int solve(int i,int m,int n,vector<string> &s){
+//         if(i==s.size() || !(m+n))     return 0;
+        
+//         if(dp[i][m][n]!=0)    return dp[i][m][n];
+//         int zero = count(s[i].begin(),s[i].end(),'0');
+//         int one = count(s[i].begin(),s[i].end(),'1');
+        
+//         int take = 0;
+//         if(zero<=m and one<=n){
+//             take = 1 + solve(i+1,m-zero,n-one,s);
+//         }
+//         int noTake = solve(i+1,m,n,s);
+        
+//         return dp[i][m][n] = max(noTake,take);
+//     }
+// public:
+//     int findMaxForm(vector<string>& strs, int m, int n) {
+//         return solve(0,m,n,strs);        
+//     }
+// };
+
+// // DP ITRATIVE:
 class Solution {
-    int dp[607][107][107];
-private:
-    int solve(int i,int m,int n,vector<string> &s){
-        if(i==s.size() || !(m+n))     return 0;
-        
-        if(dp[i][m][n]!=0)    return dp[i][m][n];
-        int zero = count(s[i].begin(),s[i].end(),'0');
-        int one = count(s[i].begin(),s[i].end(),'1');
-        
-        int take = 0;
-        if(zero<=m and one<=n){
-            take = 1 + solve(i+1,m-zero,n-one,s);
-        }
-        int noTake = solve(i+1,m,n,s);
-        
-        return dp[i][m][n] = max(noTake,take);
-    }
+    int dp[107][107];
 public:
     int findMaxForm(vector<string>& strs, int m, int n) {
-        return solve(0,m,n,strs);        
+        for(auto &it:strs){
+            int zero = count(it.begin(),it.end(),'0');
+            int one = count(it.begin(),it.end(),'1');
+            for(int i=m;i>=zero;i--){        // ZERO
+                for(int j=n;j>=one;j--){    // ONE
+                    dp[i][j] = max(dp[i-zero][j-one] + 1,dp[i][j]);
+                }
+            }
+        }
+        return dp[m][n];
     }
 };
