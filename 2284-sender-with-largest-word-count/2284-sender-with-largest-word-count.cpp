3f04310caf3047469f67d9902a6490a310a78477
Harshvardhan Singh Negi <81630801+Harshvardhan-sN@users.noreply.github.com>
2022-05-29 16:15:29 +0530

    Time: 372 ms (50.00%), Space: 75.2 MB (50.00%) - LeetHub

--- 2284-sender-with-largest-word-count/2284-sender-with-largest-word-count.cpp
+++ 2284-sender-with-largest-word-count/2284-sender-with-largest-word-count.cpp
@@ -2,29 +2,49 @@ class Solution {
 public:
 string largestWordCount(vector<string> & m, vector<string> & sd) {
         int msz= m.size();
-        map<string, int> mp;
-        set<string> st;
-        set<string> st2;
-        for(int i=0; i<msz; i++){
-            int sz= m[i].length();
-            int ct=1;
-            for(int j=0; j<sz; j++)
-            {
-                if(m[i][j]==' ')
-                    ct++;
-            }    
-            mp[sd[i]] += ct;
-        }    
-        int mx=0;
-        for(auto it= mp.begin(); it!=mp.end(); it++){
-            if(it->second > mx)
-                mx = it->second;
-        }
-        for(auto it= mp.begin(); it!=mp.end(); it++){
-            if(it->second==mx){
-                st.insert(it->first);
-            }    
-        }
-        return *(--st.end());
-    }
+int i, j, ct;
+map<string, int> mp;
+set<string> st;
+set<string> st2;
+
+
+for(i=0; i<msz; i++)
+{
+    int sz= m[i].length();
+    ct=1;
+    //cout<<"sz:"<<sz;
+    for(j=0; j<sz; j++)
+    {
+        if(m[i][j]==' ')
+           ct++;
+    }    
+     mp[sd[i]]+=ct;
+    
+}    
+
+int mx=0;
+for(auto it= mp.begin(); it!=mp.end(); it++)
+{
+    //cout<<"sender: "<<it->first<<endl;
+    //cout<<"count: "<<it->second;
+    // cout<<endl;
+    if(it->second >=mx)
+        mx= it->second;
+}
+for(auto it= mp.begin(); it!=mp.end(); it++)
+{
+    string send= it->first;
+    if(mp[it->first]==mx)
+    {
+        //cout<<"\nsender: "<<it->first<<endl;
+        
+        st.insert(it->first);
+    }    
+}
+// if(st2.size()>0)
+//     return *st2.rbegin();
+
+
+return *(st.rbegin());
+}
 };
