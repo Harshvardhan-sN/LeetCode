d5f0ed53ae986dbbc5eee83b45598e69771cc17f
Harshvardhan Singh Negi <81630801+Harshvardhan-sN@users.noreply.github.com>
2022-06-06 21:41:50 +0530

    Time: 19 ms (59.51%), Space: 13.2 MB (82.43%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -1,28 +1,27 @@
 class Solution {
 public:
+    bool arr[201][201];
     void setZeroes(vector<vector<int>>& v1) {
         int n = v1.size();
         int m = v1[0].size();
-        int arr[n][m];
-        memset(arr,-1,sizeof(arr));
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(v1[i][j]==0){
                     int row = i;
                     int col = j;
-                    while(row<n)    arr[row++][col] = 0;
+                    while(row<n)    arr[row++][col] = 1;
                     row = i;
-                    while(row>=0)    arr[row--][col] = 0;
+                    while(row>=0)    arr[row--][col] = 1;
                     row = i;
-                    while(col<m)    arr[row][col++] = 0;
+                    while(col<m)    arr[row][col++] = 1;
                     col = j;
-                    while(col>=0)    arr[row][col--] = 0;
+                    while(col>=0)    arr[row][col--] = 1;
                 }
             }
         }
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(arr[i][j]==0)    v1[i][j] = 0;
+                if(arr[i][j]==1)    v1[i][j] = 0;
             }
         }
     }
