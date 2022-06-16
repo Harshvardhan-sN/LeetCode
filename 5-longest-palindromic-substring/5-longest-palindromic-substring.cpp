c470d7cab6abf014c7dc334e8c8568e67bbf6c8e
Harshvardhan Singh Negi <81630801+Harshvardhan-sN@users.noreply.github.com>
2022-06-17 00:16:22 +0530

    Time: 46 ms (69.03%), Space: 21.3 MB (47.90%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -9,7 +9,7 @@ public:
         for(int mid = 0; mid<n; mid++){
             for(int x = 0; mid-x>=0 and mid+x<n; x++){
                 if(s[mid-x]!=s[mid+x])  break;
-                int len = 2*x + 1;
+                int len = (x<<1) + 1;
                 if(len > maxLen){
                     maxLen = len;
                     result = s.substr(mid-x,len);
@@ -20,7 +20,7 @@ public:
         for(int mid = 0; mid<n-1; mid++){
             for(int x = 1; mid-x+1>=0 and mid+x<n; x++){
                 if(s[mid-x+1]!=s[mid+x])    break;
-                int len = 2*x;
+                int len = (x<<1);
                 if(len > maxLen){
                     maxLen = len;
                     result = s.substr(mid-x+1,len);
