adb4e972c6685de6209e603388f5ef61ceea300a
Harshvardhan Singh Negi <81630801+Harshvardhan-sN@users.noreply.github.com>
2022-06-17 21:25:30 +0530

    Time: 11 ms (83.13%), Space: 21.3 MB (23.37%) - LeetHub

--- 968-binary-tree-cameras/968-binary-tree-cameras.cpp
+++ 968-binary-tree-cameras/968-binary-tree-cameras.cpp
@@ -12,27 +12,21 @@
 class Solution {
 private:
     int ans = 0;
-    set<TreeNode*> s1;
-    void dfs(TreeNode *node, TreeNode* parent){
-        if(node){
-            dfs(node->left,node);
-            dfs(node->right,node);
-            
-                if((!parent and !s1.count(node)) || 
-                    (!s1.count(node->left)) || (!s1.count(node->right))){
-                ans++;
-                s1.insert(node);
-                s1.insert(parent);
-                s1.insert(node->left);
-                s1.insert(node->right);
-            }
+    int dfs(TreeNode* node){
+        if(!node)   return 1;
+        int Left = dfs(node->left); 
+        int Right = dfs(node->right);
+        
+        if(!Left || !Right){
+            ans++;
+            return 2;
         }
+        if(Left==2 || Right==2)     return 1;
+        
+        return 0;
     }
 public:
     int minCameraCover(TreeNode* root) {
-        if(!root)    return 0;
-        s1.insert(nullptr);
-        dfs(root,nullptr);
-        return ans;
+        return (!dfs(root) ? ++ans : ans);
     }
 };
