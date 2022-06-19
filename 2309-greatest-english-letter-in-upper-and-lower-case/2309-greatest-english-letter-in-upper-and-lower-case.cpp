6068c0a414a22f2ec44b9707993b5510792a2142
Harshvardhan Singh Negi <81630801+Harshvardhan-sN@users.noreply.github.com>
2022-06-19 09:55:56 +0530

    Time: 157 ms (20.00%), Space: 6.4 MB (100.00%) - LeetHub

--- 2309-greatest-english-letter-in-upper-and-lower-case/2309-greatest-english-letter-in-upper-and-lower-case.cpp
+++ 2309-greatest-english-letter-in-upper-and-lower-case/2309-greatest-english-letter-in-upper-and-lower-case.cpp
@@ -3,15 +3,18 @@ public:
     string greatestLetter(string s) {
         int n = s.length();
         string ans = "";
+        
         for(int i=0; i<n; i++){
             for(int j=i+1; j<n; j++){
+                // Diff of lower and upper is 32
                 if(abs(s[i]-s[j])==32){
                     string req = "";
-                    req += (s[i]>s[j]?s[j]:s[i]);
-                    ans = max(ans,req);
+                    req += (s[i]>s[j] ? s[j] : s[i]);  // Checking lower and upper
+                    ans = max(ans,req);     // greatest Char
                 }    
             }
         }
+        
         return ans;
     }
 };
