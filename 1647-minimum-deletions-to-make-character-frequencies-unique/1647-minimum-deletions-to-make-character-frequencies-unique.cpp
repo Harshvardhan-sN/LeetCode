abe0d9eb300e4b85faaa06e6a5587fec8019d666
Harshvardhan Singh Negi <81630801+Harshvardhan-sN@users.noreply.github.com>
2022-06-29 00:36:55 +0530

    Time: 73 ms (68.07%), Space: 17.3 MB (81.59%) - LeetHub

---
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int minDeletions(string s) {
+        int arr[26] = {0};
+        for(auto &it:s)     arr[it-'a']++;
+        sort(arr,arr+26);
+        unordered_set<int> s1;
+        int ans = 0;
+        for(int i=0;i<26;i++){
+            while(arr[i]>0 and s1.count(arr[i])){
+                arr[i]--;
+                ans++;
+            }
+            s1.insert(arr[i]);
+        }
+        return ans;
+    }
+};
