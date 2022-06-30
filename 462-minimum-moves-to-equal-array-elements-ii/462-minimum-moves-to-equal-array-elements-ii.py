31f2c286fdba6ba0083b3f89ea59a52959a47c99
Harshvardhan Singh Negi <81630801+Harshvardhan-sN@users.noreply.github.com>
2022-06-30 18:27:12 +0530

    Time: 142 ms (21.39%), Space: 15.4 MB (79.18%) - LeetHub

--- 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.py
+++ 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.py
@@ -1,11 +1,7 @@
 class Solution:
     def minMoves2(self, v1: List[int]) -> int:
         v1.sort();
-        l1 = v1[len(v1)>>1]
-        ans1 = 0
+        l1, ans1 = v1[len(v1)>>1],0
         for i in v1:
-            if l1>i:
-                ans1 += l1-i
-            else:
-                ans1 += i-l1
-        return ans1
+            ans1 += abs(i-l1)
+        return ans1;
