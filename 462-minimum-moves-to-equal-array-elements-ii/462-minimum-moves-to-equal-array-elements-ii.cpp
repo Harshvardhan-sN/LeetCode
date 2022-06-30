f7674bdf5ec3a4eb7052f51a62de91f68c1ff0cf
Harshvardhan Singh Negi <81630801+Harshvardhan-sN@users.noreply.github.com>
2022-06-30 18:21:42 +0530

    Time: 25 ms (14.20%), Space: 10.8 MB (92.09%) - LeetHub

--- 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.cpp
+++ 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.cpp
@@ -5,7 +5,8 @@ public:
         int l1 = v1[ v1.size()>>1];
         int ans1 = 0;
         for(auto &it:v1){
-            ans1 += abs(it-l1);
+            if (it>l1)      ans1 += it-l1;
+            else            ans1 += l1-it;
         }
         return ans1;
     }
