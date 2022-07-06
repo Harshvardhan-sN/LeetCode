47b1206bee661125f68c4eb17145769a401509f5
Harshvardhan Singh Negi <81630801+Harshvardhan-sN@users.noreply.github.com>
2022-07-06 19:46:47 +0530

    Time: 617 ms (5.64%), Space: 313.3 MB (9.61%) - LeetHub

---
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -0,0 +1,13 @@
+class Solution {
+private:
+    int fun(int n,vector<int> &dp){
+        if (n<=1)   return n;
+        if (dp[n]!=-1)      return dp[n];
+        return dp[n] = fun(n-1,dp) + fun(n-2,dp);
+    }
+public:
+    int fib(int n) {
+        vector<int> dp(1e7+7,-1);
+        return fun(n,dp);
+    }
+};
