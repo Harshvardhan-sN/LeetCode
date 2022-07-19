d6edd0681ba985219937d116596088d6ce626410
Harshvardhan Singh Negi <81630801+Harshvardhan-sN@users.noreply.github.com>
2022-07-19 07:23:48 +0530

    Time: 4 ms (25.27%), Space: 6.6 MB (31.97%) - LeetHub

---
+++ 118-pascals-triangle/118-pascals-triangle.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<vector<int>> generate(int a) {
+        vector<vector<int>> v1(a);
+        for(int i=0;i<a;i++){
+            v1[i].resize(i+1);
+            v1[i][0]=v1[i][i]=1;
+            for(int j=1;j<i;j++){
+                v1[i][j]=v1[i-1][j-1]+v1[i-1][j];
+            }
+        }
+        return v1;
+    }
+};
