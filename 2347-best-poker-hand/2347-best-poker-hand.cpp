423fe0d5b00f2b158ad1e0d4770e8849aae78509
Harshvardhan Singh Negi <81630801+Harshvardhan-sN@users.noreply.github.com>
2022-07-23 23:46:17 +0530

    Time: 7 ms (25.00%), Space: 10 MB (50.00%) - LeetHub

--- 2347-best-poker-hand/2347-best-poker-hand.cpp
+++ 2347-best-poker-hand/2347-best-poker-hand.cpp
@@ -1,17 +1,14 @@
 class Solution {
 public:
     string bestHand(vector<int>& ranks, vector<char>& suits) {
-        map<int,int> m1;
-        int mn1 = INT_MIN;
-        int S = count(begin(suits), end(suits), suits[0]);
         int n = ranks.size();
-        for(int i=0;i<n;i++){
-            m1[ranks[i]]++;
-            mn1 = max(mn1,m1[ranks[i]]);
-        }
-        if(S==n)     return "Flush";
-        if(mn1>=3)      return "Three of a Kind";
-        if(mn1==2)      return "Pair";
+        int all_same = count(begin(suits), end(suits), suits[0]);       
+        vector<int> v1(14, 0);
+        for(int i = 0; i < n; i++)    v1[ranks[i]]++;
+        int maxi = *max_element(begin(v1), end(v1));
+        if (all_same == n)     return "Flush";
+        if (maxi >= 3)      return "Three of a Kind";
+        if (maxi == 2)      return "Pair";
         return "High Card";
     }
 };
