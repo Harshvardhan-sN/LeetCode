47f25c3d6e4e741c7a2c33475966afc7c9df1f1c
Harshvardhan Singh Negi <81630801+Harshvardhan-sN@users.noreply.github.com>
2022-07-28 09:55:33 +0530

    Time: 10 ms (74.53%), Space: 7.4 MB (7.79%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -1,8 +1,13 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(begin(s),end(s));
-        sort(begin(t),end(t));
-        return s==t;
+        vector<int> v1(26,0);
+        int n = t.size(), m = s.size();
+        if(n != m)      return false;
+        for(int i = 0; i < n; i++){
+            v1[s[i]-'a']++;
+            v1[t[i]-'a']--;
+        }
+        return count(begin(v1),end(v1),0)==26;
     }
 };
