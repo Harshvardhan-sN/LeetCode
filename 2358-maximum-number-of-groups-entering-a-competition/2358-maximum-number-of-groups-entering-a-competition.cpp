79bfeeadc0e7bc400b679fef68870e9e3581b8a3
Harshvardhan Singh Negi <81630801+Harshvardhan-sN@users.noreply.github.com>
2022-08-01 08:29:44 +0530

    Time: 123 ms (100.00%), Space: 56.6 MB (100.00%) - LeetHub

--- 2358-maximum-number-of-groups-entering-a-competition/2358-maximum-number-of-groups-entering-a-competition.cpp
+++ 2358-maximum-number-of-groups-entering-a-competition/2358-maximum-number-of-groups-entering-a-competition.cpp
@@ -1,6 +1,11 @@
 class Solution {
 public:
-    int maximumGroups(vector<int>& A) {
-        return (int)(sqrt(A.size() * 2 + 0.25) - 0.5);
+    int maximumGroups(vector<int>& g) {        
+        int n = g.size(), m = 0, i = 1, ans = 0;        
+        while(m < n){
+            m += i++;
+            ans++;
+        }
+        return m > n ? --ans : ans;
     }
 };
