15c3b9f55c4af4085b6e559494af5cf20b14db99
Harshvardhan Singh Negi <81630801+Harshvardhan-sN@users.noreply.github.com>
2022-12-03 22:31:42 +0530

    Time: 15 ms (82.09%), Space: 8.5 MB (40.07%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -1,35 +1,35 @@
-// class Solution {
-// public:
-//     static bool cmp(pair<char, int> p1, pair<char, int> p2){
-//         return p1.second>p2.second;
-//     }
-//     string frequencySort(string s) {
-//         unordered_map<char, int> m1;
-//         vector<pair<char, int>> v1;
-//         for(char &ch: s)    m1[ch]++;
-//         for(auto &[l,r]: m1)    v1.emplace_back(l,r);
-//         sort(begin(v1), end(v1), cmp);
-//         string ans = "";
-//         for(auto &[l,r]: v1){
-//             int k = r;
-//             while(r--)      ans += l;
-//         }
-//         return ans;
-//     }
-// };
 class Solution {
 public:
+    static bool cmp(pair<char, int> p1, pair<char, int> p2){
+        return p1.second>p2.second;
+    }
     string frequencySort(string s) {
-        unordered_map<char,int> m1;
-        for(auto &it:s)     m1[it]++;
-        multimap<int,char> s1;
-        for(auto &it:m1)    s1.insert({it.second,it.first});
+        unordered_map<char, int> m1;
+        vector<pair<char, int>> v1;
+        for(char &ch: s)    m1[ch]++;
+        for(auto &[l,r]: m1)    v1.emplace_back(l,r);
+        sort(begin(v1), end(v1), cmp);
         string ans = "";
-        for(auto it = s1.rbegin();it!=s1.rend();it++) {
-            for(int i=0;i<it->first;i++){
-                ans += it->second;
-            }
+        for(auto &[l,r]: v1){
+            int k = r;
+            while(r--)      ans += l;
         }
         return ans;
     }
 };
+// class Solution {
+// public:
+//     string frequencySort(string s) {
+//         unordered_map<char,int> m1;
+//         for(auto &it:s)     m1[it]++;
+//         multimap<int,char> s1;
+//         for(auto &it:m1)    s1.insert({it.second,it.first});
+//         string ans = "";
+//         for(auto it = s1.rbegin();it!=s1.rend();it++) {
+//             for(int i=0;i<it->first;i++){
+//                 ans += it->second;
+//             }
+//         }
+//         return ans;
+//     }
+// };
