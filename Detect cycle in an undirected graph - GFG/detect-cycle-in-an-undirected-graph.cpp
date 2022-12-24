89d5614f204d493bedf50535dee274cb863e13ec
Harshvardhan Singh Negi <81630801+Harshvardhan-sN@users.noreply.github.com>
2022-12-24 12:23:54 +0530

    Added solution - LeetHub

--- Detect cycle in an undirected graph - GFG/detect-cycle-in-an-undirected-graph.cpp
+++ Detect cycle in an undirected graph - GFG/detect-cycle-in-an-undirected-graph.cpp
@@ -5,7 +5,7 @@ using namespace std;
 // } Driver Code Ends
 class Solution {
     private:
-    bool bfs(int V, int source, vector<int> adj[], vector<bool> vis){
+    bool bfs(int source, vector<int> adj[], vector<bool> vis){
         queue<pair<int,int>> que;
         que.push({source, -1});
         vis[source] = 1;
@@ -25,7 +25,17 @@ class Solution {
         }
         return 0;
     }
-    bool dfs(int V, int S, vector<int> adj[], vector<bool> vis){
+    bool dfs(int source, int parent, vector<int> adj[], vector<bool> &vis){
+        vis[source] = 1;
+        for(int &it: adj[source]){
+            if(it!=parent){
+                if(vis[it])     return 1;
+                else            dfs(it, source, adj, vis);
+            }
+        }
+        return 0;
+    }
+    bool dfsWithStack(int S, vector<int> adj[], vector<bool> vis){
         stack<pair<int, int>> sta;
         sta.push({S, -1});
         vis[S] = 1;
@@ -48,10 +58,11 @@ class Solution {
   public:
     // Function to detect cycle in an undirected graph.
     bool isCycle(int V, vector<int> adj[]) {
-        vector<bool> vis(V, 0);
         for(int i=0; i<V; i++){
-            // if(bfs(V, i, adj, vis))         return 1;
-            if(dfs(V, i, adj, vis))         return 1;
+            // if(bfs(i, adj, vis))         return 1;
+            // if(dfsWithStack(i, adj, vis))         return 1;
+            vector<bool> vis(V, 0);
+            if(dfs(i, -1, adj, vis))            return 1;
         }
         return 0;
     }
