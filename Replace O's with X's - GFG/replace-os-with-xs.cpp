c1651f3fb2da245e77a933eddb0d4495a3c1e464
Harshvardhan Singh Negi <81630801+Harshvardhan-sN@users.noreply.github.com>
2022-12-25 12:54:36 +0530

    Added solution - LeetHub

--- Replace O's with X's - GFG/replace-os-with-xs.cpp
+++ Replace O's with X's - GFG/replace-os-with-xs.cpp
@@ -10,6 +10,17 @@ using namespace std;
 class Solution{
     int dis[5] = {0, -1, 0, 1, 0};
 public:
+void dfs(int r, int c, int n, int m, vector<vector<char>> &mat, vector<vector<char>> &res, vector<vector<bool>> &vis){
+    vis[r][c] = 1;
+    for(int i=0; i<4; i++){
+        int nR = r + dis[i];
+        int nC = c + dis[i+1];
+        if(nR>=0 and nC>=0 and nR<n and nC<m and !vis[nR][nC] and mat[nR][nC]=='O'){
+            res[nR][nC] = 'O';
+            dfs(nR, nC, n, m, mat, res, vis);
+        }
+    }
+}
     vector<vector<char>> fill(int n, int m, vector<vector<char>> mat)
     {
         vector<vector<bool>> vis(n, vector<bool>(m, 0));
@@ -19,27 +30,28 @@ public:
         for(int j=0; j<m; j++){
             if((i==0 || i==n-1) || (i>0 and i<n-1 and (j==0 || j==m-1))){
                 if(mat[i][j]=='O'){
-                    q1.push({i, j});
-                    vis[i][j] = 1;
+                    // q1.push({i, j});
+                    // vis[i][j] = 1;
                     res[i][j] = 'O';
+                    dfs(i, j, n, m, mat, res, vis);
                 }
             }
         }
     }
-    while(!q1.empty()){
-        int r = q1.front().first;
-        int c = q1.front().second;
-        q1.pop();
-        for(int i=0; i<4; i++){
-            int nR = r + dis[i];
-            int nC = c + dis[i+1];
-            if(nR>=0 and nC>=0 and nR<n and nC<m and !vis[nR][nC] and mat[nR][nC]=='O'){
-                res[nR][nC] = 'O';
-                vis[nR][nC] = 1;
-                q1.push({nR, nC});
-            }
-        }
-    }
+    // while(!q1.empty()){
+    //     int r = q1.front().first;
+    //     int c = q1.front().second;
+    //     q1.pop();
+    //     for(int i=0; i<4; i++){
+    //         int nR = r + dis[i];
+    //         int nC = c + dis[i+1];
+    //         if(nR>=0 and nC>=0 and nR<n and nC<m and !vis[nR][nC] and mat[nR][nC]=='O'){
+    //             res[nR][nC] = 'O';
+    //             vis[nR][nC] = 1;
+    //             q1.push({nR, nC});
+    //         }
+    //     }
+    // }
     return res;
     }
 };
