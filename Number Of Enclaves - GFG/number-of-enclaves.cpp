7f01211e7017075a15628ebb2fa7c3fe4ee62a1c
Harshvardhan Singh Negi <81630801+Harshvardhan-sN@users.noreply.github.com>
2022-12-25 15:16:40 +0530

    Added solution - LeetHub

--- Number Of Enclaves - GFG/number-of-enclaves.cpp
+++ Number Of Enclaves - GFG/number-of-enclaves.cpp
@@ -23,21 +23,47 @@ class Solution {
             }
         }
     }
+    void bfs(queue<pair<int, int>> &q1, int n, int m, vector<vector<int>> &grid, vector<vector<bool>> &vis){
+        while(!q1.empty()){
+            int r = q1.front().first;
+            int c = q1.front().second;
+            q1.pop();
+            for(int i=0; i<4; i++){
+                int nR = r + dis[i];
+                int nC = c + dis[i+1];
+                if(nR>=0 and nC>=0 and nR<n and nC<m and !vis[nR][nC] and grid[nR][nC]){
+                    // grid[nR][nC] = 0;
+                    vis[nR][nC] = 1;
+                    q1.push({nR, nC});
+                }
+            }
+        }   
+    }
     int numberOfEnclaves(vector<vector<int>> &grid) {
         int n = grid.size(), m = grid[0].size(), res = 0;
         vector<vector<bool>> vis(n, vector<bool>(m, 0));
+        queue<pair<int, int>> q1;
         for(int i=0; i<n; i++){
             for(int j=0; j<m; j++){
                 if( (i==0 || i==n-1) || (i>0 and i<n-1 and (j==0 || j==m-1)) ){
                     if(!vis[i][j] and grid[i][j]){
-                        grid[i][j] = 0;
-                        dfs(i, j, n, m, grid, vis);
+                        // grid[i][j] = 0;
+                    
+                        // // for dfs
+                        // dfs(i, j, n, m, grid, vis);
+                        
+                        // // for bfs
+                        q1.push({i, j});
+                        vis[i][j] = 1;
                     }
                 }
             }
         }
-        for(auto arr: grid){
-            for(auto it: arr)      res += it;
+        bfs(q1, n, m, grid, vis);
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                res += grid[i][j]>vis[i][j];
+            }
         }
         return res;
     }
