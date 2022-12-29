43a173984eb5635e23279499eebc2fa84d15e2b5
Harshvardhan Singh Negi <81630801+Harshvardhan-sN@users.noreply.github.com>
2022-12-29 08:11:21 +0530

    Added solution - LeetHub

--- Prerequisite Tasks - GFG/prerequisite-tasks.cpp
+++ Prerequisite Tasks - GFG/prerequisite-tasks.cpp
@@ -6,31 +6,38 @@ using namespace std;
 class Solution {
 public:
 	bool isPossible(int N, vector<pair<int, int> >& prerequisites) {
-	    vector<int> inDegree(N, 0);
+	    vector<int> InDegree(N, 0);
 	    vector<int> adj[N];
+	    // create a adjacency matrix
 	    for(auto &it: prerequisites){
+	        // it.first ---> it.second
+	        //    u     --->    v
 	        adj[it.first].push_back(it.second);
 	    }
+	    // count number of inDegrees for each node
 	    for(int i=0; i<N; i++){
-	        for(auto &it: adj[i]){
-	            inDegree[it]++;
-	        }
+	        for(auto &it: adj[i])   InDegree[it]++;
 	    }
 	    queue<int> q1;
-	    for(int it=0; it<N; it++){
-	        if(inDegree[it]==0)    q1.push(it);
+	    // insert node on queue with InDegree 0
+	    for(int i=0; i<N; i++){
+	        if(InDegree[i]==0)      q1.push(i);
 	    }
-	    vector<int> res;
+	    // bfs for toposort
+	    vector<int> toposort;
 	    while(!q1.empty()){
-	        int child = q1.front();
+	        int from = q1.front();
+	        toposort.push_back(from);    // linear order of sort
 	        q1.pop();
-	        res.push_back(child);
-	        for(auto &it: adj[child]){
-	            inDegree[it]--;
-	            if(inDegree[it]==0)     q1.push(it);
+	        for(auto &it: adj[from]){
+	            InDegree[it]--;
+	            if(InDegree[it]==0)     q1.push(it);
 	        }
 	    }
-	    return (res.size()==N);
+	    // if toposort exist then 
+	    // there is a schedule to complete the task in orders
+	    if(toposort.size()==N)      return 1;
+	    return 0;
 	}
 };
 
