1ca6de9bddddf0d9304f84c889ef2db1167bd3fa
Harshvardhan Singh Negi <81630801+Harshvardhan-sN@users.noreply.github.com>
2022-12-30 12:01:18 +0530

    Added solution - LeetHub

---
+++ Shortest path in Directed Acyclic Graph - GFG/shortest-path-in-directed-acyclic-graph.cpp
@@ -0,0 +1,74 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+class Solution {
+  public:
+    void dfs(int S, vector<int> &vis, vector<pair<int, int>> adj[], stack<int> &st){
+        vis[S] = 1;
+        for(auto &it: adj[S]){
+            if(!vis[it.first]){
+                dfs(it.first, vis, adj, st);
+            }
+        }
+        st.push(S);
+    }
+    vector<int> shortestPath(int N,int M, vector<vector<int>>& edges){
+        stack<int> st;
+        vector<pair<int, int>> adj[N];
+        vector<int> vis(N, 0), path(N, 101);
+        for(auto &it: edges){
+            adj[it[0]].push_back({it[1],it[2]});
+        }
+        for(int i=0; i<N; i++){
+            if(!vis[i]){
+                dfs(i, vis, adj, st);
+            }
+        }
+        path[0] = 0;
+        while(!st.empty()){
+            int top = st.top();
+            st.pop();
+            int currDistance = path[top];
+            for(auto &it: adj[top]){
+                path[it.first] = min(path[it.first], currDistance+it.second);
+            }
+        }
+        for(auto &it: path){
+            if(it==101)     it = -1;
+        }
+        return path;
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, m;
+        cin >> n >> m;
+        vector<vector<int>> edges;
+        for(int i=0; i<m; ++i){
+            vector<int> temp;
+            for(int j=0; j<3; ++j){
+                int x; cin>>x;
+                temp.push_back(x);
+            }
+            edges.push_back(temp);
+        }
+        Solution obj;
+        vector<int> res = obj.shortestPath(n, m, edges);
+        for (auto x : res) {
+            cout << x << " ";
+        }
+        cout << "\n";
+    }
+}
+
+// } Driver Code Ends
