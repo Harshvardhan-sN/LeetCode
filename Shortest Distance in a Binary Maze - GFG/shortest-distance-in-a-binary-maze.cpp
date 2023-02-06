a3ddda679ac9dc0cc4c85655a9bb1587ca2b8a63
Harshvardhan Singh Negi <81630801+Harshvardhan-sN@users.noreply.github.com>
2023-02-06 21:37:09 +0530

    Added solution - LeetHub

--- Shortest Distance in a Binary Maze - GFG/shortest-distance-in-a-binary-maze.cpp
+++ Shortest Distance in a Binary Maze - GFG/shortest-distance-in-a-binary-maze.cpp
@@ -21,17 +21,19 @@ bool isValidDirection(int X, int Y, int n, int m) {return (X >= 0 && Y >= 0 && X
     int shortestPath(vector<vector<int>> &grid, pair<int, int> source,
                      pair<int, int> destination) {
         int n = grid.size(), m = grid[0].size();
+    set<pair<int, pair<int, int>>> q1;
     // queue<val> q1;
-    priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, greater<pair<int, pair<int, int>>>> q1;
+    // priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, greater<pair<int, pair<int, int>>>> q1;
     vector<vector<bool>> vis(n, vector<bool> (m, 0));
     vis[source.first][source.second] = 1;
     // q1.push({source.first, source.second, 0});
-    q1.push({0, source});
+    q1.insert({0, source});
     while(q1.size()){
-        int x = q1.top().second.first;
-        int y = q1.top().second.second;
-        int cost = q1.top().first;
-        q1.pop();
+        auto it = q1.begin();
+        int x = it->second.first;
+        int y = it->second.second;
+        int cost = it->first;
+        q1.erase(it);
         if(x == destination.first and y == destination.second)
             return cost;
         for(int i = 0; i < 4; i++){
@@ -39,7 +41,7 @@ bool isValidDirection(int X, int Y, int n, int m) {return (X >= 0 && Y >= 0 && X
             int dY = y + dy[i];
             if(isValidDirection(dX, dY, n, m) and !vis[dX][dY] and grid[dX][dY]==1){
                 vis[dX][dY] = 1;
-                q1.push({cost + 1, {dX, dY}});
+                q1.insert({cost + 1, {dX, dY}});
             }
         }
     }
