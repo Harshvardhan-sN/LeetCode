1485aa10cb66c2be54e28413dce207910129d4ce
Harshvardhan Singh Negi <81630801+Harshvardhan-sN@users.noreply.github.com>
2023-02-08 23:53:49 +0530

    Time: 2872 ms (5.01%), Space: 340.4 MB (6.22%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        queue<int> q1;
+        int n = nums.size();
+        vector<int> dp(n + 1, -1);
+        q1.push(0);
+        while(q1.size()){
+            int position = q1.front();
+            q1.pop();
+            if(position == nums.size() - 1)  return 1;
+            if(dp[position] != -1)   continue; 
+            for(int i = nums[position]; i > 0; i--){
+                if(i + position == nums.size() - 1)  return 1;
+                if(i + position < nums.size() and nums[i + position]!=0){
+                    q1.push(i + position);
+                    dp[position] = i + position;
+                }
+            }
+        }
+        return 0;
+    }
+};
