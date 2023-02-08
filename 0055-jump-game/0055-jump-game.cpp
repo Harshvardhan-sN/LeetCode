534fb672167665b97e2003cdb161f498610cfe05
Harshvardhan Singh Negi <81630801+Harshvardhan-sN@users.noreply.github.com>
2023-02-09 00:27:30 +0530

    Time: 72 ms (57.75%), Space: 48.3 MB (94.28%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,12 +1,15 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-        if(nums.size() > 1 and nums[0] == 0)    return 0;
-        int n = nums.size() - 1;
-        for(int i = n; i >= 0; i--)
-            if(i + nums[i] >= n)
-                n = i;
-        return n == 0;
+        int reachable = 0, n = nums.size();
+        if(n > 1 and nums[0] == 0)  return 0;
+        for(int i = 0; i < n - 1; i++){
+            if(nums[i] == 0)    continue;
+            if(i > reachable)   return 0;
+            if(nums[i] + i > reachable)
+                reachable = nums[i] + i;
+        }
+        return reachable >= n - 1;
     }
 };
 
