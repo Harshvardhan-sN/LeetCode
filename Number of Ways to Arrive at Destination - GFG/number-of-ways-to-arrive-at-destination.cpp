fc1f086831e1f695e579135680f7f40dab8597e4
Harshvardhan Singh Negi <81630801+Harshvardhan-sN@users.noreply.github.com>
2023-02-12 23:43:23 +0530

    Added solution - LeetHub

--- Number of Ways to Arrive at Destination - GFG/number-of-ways-to-arrive-at-destination.cpp
+++ Number of Ways to Arrive at Destination - GFG/number-of-ways-to-arrive-at-destination.cpp
@@ -9,39 +9,35 @@ class Solution {
   public:
     int countPaths(int n, vector<vector<int>>& roads) {
         int res = 0, minCost = -1, mod = 1e9 + 7;
-        // priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, greater<pair<int, pair<int, int>>>> pq;
-        queue<pair<int, pair<int, int>>> pq;
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
         vector<vector<int>> adj[n];
         for(auto &it: roads){
             adj[it[0]].push_back({it[1], it[2]});
             adj[it[1]].push_back({it[0], it[2]});
         }
-        pq.push({0, {0, -1}});
+        pq.push({0, 0});
         vector<int> vis(n, 1e9);
+        vector<int> ways(n, 0);
+        ways[0] = 1;
         vis[0] = 0;
         while(pq.size()){
-            int cost = pq.front().first;
-            int node = pq.front().second.first;
-            int parent = pq.front().second.second;
+            int cost = pq.top().first;
+            int node = pq.top().second;
             pq.pop();
-            if(node == n - 1){
-                if(minCost == -1)
-                    minCost = cost;
-                if(minCost == cost)
-                    res++;
-                res %= mod;
-                continue;
-            }
-            if(minCost > -1 and cost > minCost) 
-                continue;
             for(auto &child: adj[node]){
-                if(child[0] != parent and cost + child[1] <= vis[child[0]]){
-                    vis[child[0]] = cost + child[1];
-                    pq.push({vis[child[0]], {child[0], node}});
+                int newCost = cost + child[1];
+                if(newCost < vis[child[0]]){
+                    vis[child[0]] = newCost;
+                    ways[child[0]] = ways[node];
+                    pq.push({newCost, child[0]});
+                }
+                else if(newCost == vis[child[0]]){
+                    ways[child[0]] += ways[node];
+                    ways[child[0]] %= mod;
                 }
             }
         }
-        return res % mod;
+        return ways[n - 1] % mod;
     }
 };
 
